bb3e2df54485cde96baed3e59a11f8e6b20276d8
Trey Feldman <tf912@nyu.edu>
2015-10-02 17:18:23 -0400

    lexical_probabilities done

--- HW4.py
+++ HW4.py
@@ -1,29 +1,26 @@
+from sys import argv
 
 
-#{'POS1': {'POS2': 3, 'POS4': 2}, 'POS2': {'POS1': 9, 'POS5': 3}}
-#dict['POS1']['POS2']
+training_file = file(argv[1], 'r')
 
-dict = {}
-prev = None
-pos = None
+input_file = file(argv[2], 'r')
 
-pos_trainer = file('training.pos', 'r')
+output_file = file(argv[3], 'w')
 
-#pos_trainer = file('training2.pos', 'r')
+
 
 
 """
-Gets input and returns a dictionary with the counts of how many times each word/pos tag succeeds another. A 'TOTAL' key
+Gets input and returns a dictionary with the counts of how many times each pos tag succeeds another. A 'TOTAL' key
 with the total number of times the word/pos tag is succeeded is included as well.
 Input:
     trainer: a standard input file provided for this assignment.
-    pos_or_word: 0 to get counts of words, 1 to get counts of pos tags
 Returns:
     A dict of the format:
     {'VB': {'PRP$': 1, 'PRP': 2, 'TOTAL': 3}, 'NN': {'.': 1, 'TOTAL': 2,'IN': 1,}, 'PRP$': {'NN': 1, 'TOTAL': 1}}
     meaning that a VB was followed by a PRP$ once and a PRP twice, NN ws followed by a . once and an IN once, etc...
 """
-def get_counts(trainer, pos_or_word):
+def get_pos_counts(trainer):
 
     dict = {}
     selection = None
@@ -31,14 +28,14 @@ def get_counts(trainer, pos_or_word):
 
     for line in trainer:
         if line.strip() != '':
-            selection = line.split()[pos_or_word]
+            selection = line.split()[1]
             if selection not in dict:
                 dict[selection] = {'TOTAL':0}
             if prev != None:
                 if dict[prev].has_key(selection):
                     dict[prev][selection] += 1
                 else:
-                    dict[prev][selection] = 1.0
+                    dict[prev][selection] = 1
                 dict[prev]['TOTAL'] += 1
             prev = selection
         else:
@@ -48,25 +45,83 @@ def get_counts(trainer, pos_or_word):
 
 
 """
+Gets input and returns a dictionary with the counts of how many times each word was labeled for each specific tag. A
+'TOTAL' key with the total number of times the tag was found is also included with each tag's dictionary.
 Input:
-    Takes a dictionary in the format returned by get_counts().
+    trainer: a standard input file provided for this assignment.
+Returns:
+    A dict of the format:
+    {'VB': {'run': 1, 'hit': 1, 'TOTAL': 2}, 'NN': {'while': 1, 'TOTAL': 4, 'job': 1, 'horse': 2}}
+    meaning that there was two words labeled VB one time: run and hit, horse was labeled a NN twice, etc...
+"""
+def get_word_counts(trainer):
+
+    dict = {}
+
+    for line in trainer:
+        if line.strip() != '':
+            line = line.split()
+            if line[1] not in dict:
+                dict[line[1]] = {'TOTAL': 1, line[0]: 1}
+            else:
+                if line[0] in dict[line[1]]:
+                    dict[line[1]][line[0]] += 1
+                else:
+                    dict[line[1]][line[0]] = 1
+                dict[line[1]]['TOTAL'] += 1
+
+
+    return dict
+
+
+"""
+Input:
+    Takes a dictionary with values containing another dictionary that include one 'TOTAL' key.
 Functionality:
-    Replaces the counts of how many times a word/pos tag succeeds another with probabilities of succeeding
-    its predecessor.
+    Replaces the counts in each value dictionary with probabilities.
+e.g.:
+    Input:
+        {VB:{'run': 2, 'bike': 2, 'TOTAL': 4}, NN:{'duck': 4, 'cow': 2, 'TOTAL': 6}}
+    Output:
+        {VB:{'run': 0.5, 'bike': 0.5, 'TOTAL': 4}, NN:{'duck': 0.666, 'cow': 0.333, 'TOTAL': 6}}
 """
 def get_probabilities(dict):
     for key in dict:
         for key2 in dict[key]:
             if key2 != 'TOTAL':
-                dict[key][key2] = dict[key][key2] / dict[key]['TOTAL']
+                dict[key][key2] = float(dict[key][key2]) / dict[key]['TOTAL']
 
     return dict
 
+"""
+Returns a dictionary with keys being the possible parts of speech and their values being the lexical likelihoods
+"""
+def lexical_probabilities(word, word_probabilities):
+    probabilities_dict = {}
+    for pos in word_probabilities:
+        if word in word_probabilities[pos]:
+            probabilities_dict[pos] = word_probabilities[pos][word]
 
-dict = get_probabilities(get_counts(pos_trainer, 1))
+    return probabilities_dict
 
-print dict['JJ']['DT']
 
+def tag_words(input, output, pos_probabilities, word_probabilities):
+    for line in input:
+        word = line[0:-1]
+
+    #TODO compute probabilities
+
+
+
+        output.write(line[0:-1] + '\t' +'tag' + '\n')
+
+
+pos_prob_dict = get_probabilities(get_pos_counts(training_file))
+training_file.seek(0)
+word_prob_dict = get_probabilities(get_word_counts(training_file))
+
+
+tag_words(input_file, output_file, pos_prob_dict, word_prob_dict)
 
 
 
